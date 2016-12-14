314ae2bd85ee87c52ac29613c7bc2248c1c386b5
Dirk Thomas <dthomas@osrfoundation.org>
2016-12-14 14:00:41 -0800

    reenable diagnostic_msgs

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -437,7 +437,6 @@ def run(args, build_function):
 
         blacklisted_package_names = [
             'actionlib_msgs',
-            'diagnostic_msgs',
             'nav_msgs',
             'shape_msgs',
             'std_srvs',
