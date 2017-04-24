f97a0a9fac2285e2666a230e85c24bae098e7475
Brian Gerkey <gerkey@osrfoundation.org>
2017-04-24 14:35:59 -0700

    debugging

--- ros2_batch_job/util.py
+++ ros2_batch_job/util.py
@@ -145,6 +145,9 @@ def log(*args, **kwargs):
     if fargs is not None or fkwargs is not None:
         fargs = fargs or []
         fkwargs = fkwargs or {}
+        print('msg: %s'%(msg))
+        print('fargs: %s'%(fargs))
+        print('kwargs: %s'%(kwargs))
         msg = msg.format(*fargs, **kwargs)
     # If the user did not provide an end kwarg, automatically clear the coloring.
     if 'end' not in kwargs:
