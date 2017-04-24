3b516ba6d50a7a9dd358a03418e8170e79667073
Brian Gerkey <gerkey@osrfoundation.org>
2017-04-24 16:41:35 -0700

    debugging

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -291,6 +291,7 @@ def run(args, build_function, blacklisted_package_names=None):
 
     info("run_ros2_batch called with args:")
     for arg in vars(args):
+        print('%s %s'%(arg, getattr(args, arg)))
         info("  - {0}={1}".format(sanitize(arg), sanitize(getattr(args, arg))))
 
     job = None
