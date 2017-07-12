433e1af328e0d47f5e9dcce4ad4ea4708d174bfb
Chris Lalancette <clalancette@openrobotics.org>
2017-07-12 17:20:57 -0400

    Cleanup the loop in create_jenkins_job.py that creates jobs.
    In the previous version of the loop, the job_data dict was
    shared amongst all jobs.  This means that later jobs could
    unintentionally inherit properties of earlier jobs.  Instead,
    introduce a small function to do the actual job creation,
    which ensures we get a fresh dict every time.  That is,
    we start off with the standard dict in data, update it with
    OS-specific stuff from the os_configs dict, and then update
    it with job-specific data passed into the create_job()
    function.  This moves duplicated logic out of the loop
    and ensures that jobs don't inherit properties from other
    jobs.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -38,6 +38,8 @@ except ImportError:
     sys.exit("Could not import symbol from ros_buildfarm, please update ros_buildfarm.")
 
 DEFAULT_REPOS_URL = 'https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos'
+DEFAULT_MAIL_RECIPIENTS = 'ros2-buildfarm@googlegroups.com'
+PERIODIC_JOB_SPEC = '30 7 * * *'
 
 template_prefix_path[:] = \
     [os.path.join(os.path.abspath(os.path.dirname(__file__)), 'job_templates')]
@@ -114,73 +116,88 @@ def main(argv=None):
     if not args.commit:
         jenkins_kwargs['dry_run'] = True
 
-    # configure os specific jobs
-    for os_name in sorted(os_configs.keys()):
+    def create_job(os_name, job_name, template_file, additional_dict):
         job_data = dict(data)
         job_data['os_name'] = os_name
         job_data.update(os_configs[os_name])
+        job_data.update(additional_dict)
+        job_config = expand_template(template_file, job_data)
+        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
+
+    # configure os specific jobs
+    for os_name in sorted(os_configs.keys()):
+        # We need the keep the paths short on Windows, so on that platform make
+        # the os_name shorter just for the jobs
+        job_os_name = os_name
+        if os_name == 'windows':
+            job_os_name = 'win'
 
         # configure manual triggered job
-        job_name = 'ci_' + os_name
-        job_data['cmake_build_type'] = 'None'
-        job_config = expand_template('ci_job.xml.em', job_data)
-        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
+        create_job(os_name, 'ci_' + os_name, 'ci_job.xml.em', {
+            'cmake_build_type': 'None',
+        })
 
         # configure a manual version of the packaging job
-        job_name = 'ci_packaging_' + os_name
-        job_data['cmake_build_type'] = 'RelWithDebInfo'
-        job_data['test_bridge_default'] = 'true'
-        job_config = expand_template('packaging_job.xml.em', job_data)
-        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
-
-        # all following jobs are triggered nightly with email notification
-        job_data['time_trigger_spec'] = '30 7 * * *'
-        # for now, skip emailing about Windows failures
-        job_data['mailer_recipients'] = 'ros2-buildfarm@googlegroups.com'
+        create_job(os_name, 'ci_packaging_' + os_name, 'packaging_job.xml.em', {
+            'cmake_build_type': 'RelWithDebInfo',
+            'test_bridge_default': 'true',
+        })
 
         # configure packaging job
-        job_name = 'packaging_' + os_name
-        job_data['cmake_build_type'] = 'RelWithDebInfo'
-        job_data['test_bridge_default'] = 'true'
-        job_config = expand_template('packaging_job.xml.em', job_data)
-        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
+        create_job(os_name, 'packaging_' + os_name, 'packaging_job.xml.em', {
+            'cmake_build_type': 'RelWithDebInfo',
+            'test_bridge_default': 'true',
+            'time_trigger_spec': PERIODIC_JOB_SPEC,
+            'mailer_recipients': DEFAULT_MAIL_RECIPIENTS,
+        })
 
-        # keeping the paths on Windows shorter
-        os_name = os_name.replace('windows', 'win')
         # configure nightly triggered job
-        job_name = 'nightly_' + os_name + '_debug'
-        if os_name == 'win':
-            job_name = job_name[0:-2]
-        job_data['cmake_build_type'] = 'Debug'
-        job_config = expand_template('ci_job.xml.em', job_data)
-        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
+        job_name = 'nightly_' + job_os_name + '_debug'
+        if os_name == 'windows':
+            job_name = job_name[:15]
+        create_job(os_name, job_name, 'ci_job.xml.em', {
+            'cmake_build_type': 'Debug',
+            'time_trigger_spec': PERIODIC_JOB_SPEC,
+            'mailer_recipients': DEFAULT_MAIL_RECIPIENTS,
+        })
 
         # configure nightly coverage job on x86 Linux only
         if os_name == 'linux':
-            job_name = 'nightly_' + os_name + '_coverage'
-            job_data['cmake_build_type'] = 'Debug'
-            job_data['enable_c_coverage_default'] = 'true'
-            job_config = expand_template('ci_job.xml.em', job_data)
-            configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
-            job_data['enable_c_coverage_default'] = 'false'
+            create_job(os_name, 'nightly_' + os_name + '_coverage', 'ci_job.xml.em', {
+                'cmake_build_type': 'Debug',
+                'enable_c_coverage_default': 'true',
+                'time_trigger_spec': PERIODIC_JOB_SPEC,
+                'mailer_recipients': DEFAULT_MAIL_RECIPIENTS,
+            })
 
         # configure nightly triggered job
-        job_name = 'nightly_' + os_name + '_release'
-        if os_name == 'win':
-            job_name = job_name[0:-4]
-        job_data['cmake_build_type'] = 'Release'
-        job_config = expand_template('ci_job.xml.em', job_data)
-        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
+        job_name = 'nightly_' + job_os_name + '_release'
+        if os_name == 'windows':
+            job_name = job_name[:15]
+        create_job(os_name, job_name, 'ci_job.xml.em', {
+            'cmake_build_type': 'Release',
+            'time_trigger_spec': PERIODIC_JOB_SPEC,
+            'mailer_recipients': DEFAULT_MAIL_RECIPIENTS,
+        })
 
         # configure nightly triggered job with repeated testing
-        job_name = 'nightly_' + os_name + '_repeated'
-        if os_name == 'win':
-            job_name = job_name[0:-5]
-        job_data['time_trigger_spec'] = '30 7 * * *'
-        job_data['cmake_build_type'] = 'None'
-        job_data['ament_test_args_default'] = '--retest-until-fail 20 --ctest-args -LE linter --'
-        job_config = expand_template('ci_job.xml.em', job_data)
-        configure_job(jenkins, job_name, job_config, **jenkins_kwargs)
+        job_name = 'nightly_' + job_os_name + '_repeated'
+        if os_name == 'windows':
+            job_name = job_name[:15]
+        create_job(os_name, job_name, 'ci_job.xml.em', {
+            'cmake_build_type': 'None',
+            'time_trigger_spec': PERIODIC_JOB_SPEC,
+            'mailer_recipients': DEFAULT_MAIL_RECIPIENTS,
+            'ament_test_args_default': '--retest-until-fail 20 --ctest-args -LE linter --',
+        })
+
+        # configure turtlebot jobs on Linux only for now
+        if os_name in ['linux', 'linux-aarch64']:
+            create_job(os_name, 'ci_turtlebot-demo_%s' % (os_name), 'ci_job.xml.em', {
+                'cmake_build_type': 'None',
+                'turtlebot_demo': True,
+                'supplemental_repos_url': 'https://raw.githubusercontent.com/ros2/turtlebot2_demo/master/turtlebot2_demo.repos',
+            })
 
     # configure the launch job
     os_specific_data = collections.OrderedDict()
@@ -195,18 +212,6 @@ def main(argv=None):
     job_data['cmake_build_type'] = 'None'
     job_config = expand_template('ci_launcher_job.xml.em', job_data)
     configure_job(jenkins, 'ci_launcher', job_config, **jenkins_kwargs)
-
-    # Run the turtlebot job on Linux only for now.
-    for os_name in ['linux', 'linux-aarch64']:
-        turtlebot_job_data = dict(data)
-        turtlebot_job_data['os_name'] = os_name
-        turtlebot_job_data.update(os_configs[os_name])
-        turtlebot_job_data['turtlebot_demo'] = True
-        # Use a turtlebot2_demo-specific repos file by default.
-        turtlebot_job_data['supplemental_repos_url'] = 'https://raw.githubusercontent.com/ros2/turtlebot2_demo/master/turtlebot2_demo.repos'
-        turtlebot_job_data['cmake_build_type'] = 'None'
-        job_config = expand_template('ci_job.xml.em', turtlebot_job_data)
-        configure_job(jenkins, 'ci_turtlebot-demo_%s'%(os_name), job_config, **jenkins_kwargs)
 
 
 if __name__ == '__main__':
