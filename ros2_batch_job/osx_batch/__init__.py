15532ab4063d1732dc6579d1d93cbaa65294b355
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-09-14 10:17:19 -0700

    use Connext 5.3.0 on MacOS

--- ros2_batch_job/osx_batch/__init__.py
+++ ros2_batch_job/osx_batch/__init__.py
@@ -59,8 +59,8 @@ class OSXBatchJob(BatchJob):
         if self.args.connext:
             # Try to find the connext env file and source it
             connext_env_file = os.path.join(
-                '/Applications', 'rti_connext_dds-5.2.3', 'resource', 'scripts',
-                'rtisetenv_x64Darwin15clang7.0.bash')
+                '/Applications', 'rti_connext_dds-5.3.0', 'resource', 'scripts',
+                'rtisetenv_x64Darwin16clang8.0.bash')
             if not os.path.exists(connext_env_file):
                 warn("Asked to use Connext but the RTI env was not found at '{0}'".format(
                     connext_env_file))
