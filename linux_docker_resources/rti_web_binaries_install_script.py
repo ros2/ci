e2784c97c699184faa587d8ca183616829d3f93a
Mikael Arguedas <mikael@osrfoundation.org>
2017-10-10 15:44:28 -0700

    Install rti security plugins (#105)
    * install connext security plugins
    
    * path full install path to keep the script connext version agnostic
    
    * raise on invalid plugin files instead of printing an error message
    
    * Update rti_web_binaries_install_script.py
    
    * simple shuffle

--- linux_docker_resources/rti_web_binaries_install_script.py
+++ linux_docker_resources/rti_web_binaries_install_script.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import argparse
+import os
 
 import pexpect
 
@@ -46,10 +47,33 @@ def install_connext(installer_path, install_directory):
             'Unexpected output while installing Connext. Last 100 chars received from installer: ' +
             '\n' + child.before)
 
+
+def install_plugin(pkg_path, install_directory):
+    child = pexpect.spawn(
+        os.path.join(install_directory, 'bin', 'rtipkginstall') + ' ' + pkg_path, encoding='utf8')
+
+    try:
+        child.expect_exact('Do you want to continue? [Y/n]:')
+        child.sendline('y')
+        child.expect(pexpect.EOF)
+
+    except (pexpect.TIMEOUT, pexpect.EOF):
+        raise RuntimeError(
+            "Unexpected output while installing Connext plugin '%s'." % pkg_path +
+            'Last 100 chars received from installer: ' +
+            '\n' + child.before)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('installer_path')
     parser.add_argument('install_directory')
+    parser.add_argument('--rtipkg_paths', nargs='*', default=[])
 
     args = parser.parse_args()
     install_connext(args.installer_path, args.install_directory)
+    # Install successful, now installing rti plugins if any
+    for plugin_path in args.rtipkg_paths:
+        if not os.path.isfile(plugin_path):
+            raise RuntimeError("RTI package '%s' not found" % plugin_path)
+        install_plugin(plugin_path, args.install_directory)
