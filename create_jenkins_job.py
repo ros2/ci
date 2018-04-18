865872ba22f68de9f3ff80e3a2a96a4ab2bbdab6
Mikael Arguedas <mikael@osrfoundation.org>
2018-04-18 10:06:01 -0700

    display console output for colcon tests as well (#150)

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -81,7 +81,7 @@ def main(argv=None):
         'use_opensplice_default': 'false',
         'use_isolated_default': 'true',
         'ament_build_args_default': '--cmake-args " -DSECURITY=ON" --event-handler console_cohesion+',
-        'ament_test_args_default': '--retest-until-pass 10',
+        'ament_test_args_default': '--retest-until-pass 10 --event-handler console_cohesion+',
         'enable_c_coverage_default': 'false',
         'dont_notify_every_unstable_build': 'false',
         'turtlebot_demo': False,
