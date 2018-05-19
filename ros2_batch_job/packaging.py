413f458149679f6826123efc118ed8ef299c196b
Mikael Arguedas <mikael@osrfoundation.org>
2018-05-18 22:04:26 -0700

    actually stop building tests on packaging jobs

--- ros2_batch_job/packaging.py
+++ ros2_batch_job/packaging.py
@@ -44,7 +44,7 @@ def build_and_test_and_package(args, job):
     ] + (['--merge-install'] if not args.isolated else []) + \
         args.build_args
 
-    cmake_args = ['" --no-warn-unused-cli"']
+    cmake_args = ['" -DBUILD_TESTING=OFF" " --no-warn-unused-cli"']
     if args.cmake_build_type:
         cmake_args.append(
             '" -DCMAKE_BUILD_TYPE=' + args.cmake_build_type + '"')
