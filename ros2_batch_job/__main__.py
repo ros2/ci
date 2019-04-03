f4ac3d4ca90fc2407299e9a944fbf7b1c1988ccd
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-04-03 15:27:06 -0700

    install custom wheel of numpy for Windows debug (#272)

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -62,7 +62,6 @@ pip_dependencies = [
     'lark-parser',
     'mock',
     'nose',
-    'numpy',
     'pep8',
     'pydocstyle',
     'pyflakes',
@@ -472,11 +471,13 @@ def run(args, build_function, blacklisted_package_names=None):
         if sys.platform == 'win32':
             if not args.packaging and args.cmake_build_type and args.cmake_build_type == 'Debug':
                 pip_packages += [
-                    'https://github.com/ros2/ros2/releases/download/lxml-archives/lxml-4.3.2-cp37-cp37dm-win_amd64.whl'
+                    'https://github.com/ros2/ros2/releases/download/lxml-archives/lxml-4.3.2-cp37-cp37dm-win_amd64.whl',
+                    'https://github.com/ros2/ros2/releases/download/numpy-archives/numpy-1.16.2-cp37-cp37dm-win_amd64.whl',
                 ]
             else:
                 pip_packages += [
-                    'lxml'
+                    'lxml',
+                    'numpy',
                 ]
         if not args.colcon_branch:
             pip_packages += colcon_packages
