94b3674fdbb89a5e7067cddda871d51fd453fced
Scott K Logan <logans@cottsay.net>
2019-04-08 17:27:32 -0700

    Modify test_args_default instead of overriding it (#275)

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -286,7 +286,8 @@ def main(argv=None):
         job_name = 'nightly_' + job_os_name + '_repeated'
         if os_name == 'windows':
             job_name = job_name[:15]
-        test_args_default = '--event-handlers console_direct+ --executor sequential --retest-until-fail 10 --ctest-args -LE linter --pytest-args -m "not linter"'
+        test_args_default = os_configs.get(os_name, data).get('test_args_default', data['test_args_default'])
+        test_args_default = test_args_default.replace('--retest-until-pass', '--retest-until-fail') + ' --ctest-args -LE linter --pytest-args -m "not linter"'
         if job_os_name == 'linux-aarch64':
             # skipping known to be flaky tests https://github.com/ros2/rviz/issues/368
             test_args_default += ' --packages-skip rviz_common rviz_default_plugins rviz_rendering rviz_rendering_tests'
