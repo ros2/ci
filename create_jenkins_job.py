ros2/ci

commit fb403d185bab5188f8bb814db49ef76c2e3d88d8

Disable sanitizers on CentOS until RHBZ#1667885 is resolved (#289)
Signed-off-by: Scott K Logan <logans@cottsay.net>

create_jenkins_job.py
@@ -121,7 +121,7 @@ def main(argv=None):
             'label_expression': 'linux',
             'shell_type': 'Shell',
             'build_args_default': '--packages-skip-by-dep qt_gui_cpp image_tools --packages-skip qt_gui_cpp image_tools ' + data['build_args_default'].replace(
-                '--cmake-args', '--cmake-args -DCMAKE_POLICY_DEFAULT_CMP0072=NEW -DPYTHON_VERSION=3.6'),
+                '--cmake-args', '--cmake-args -DCMAKE_POLICY_DEFAULT_CMP0072=NEW -DPYTHON_VERSION=3.6 -DDISABLE_SANITIZERS'),
             'test_args_default': '--packages-skip-by-dep qt_gui_cpp image_tools --packages-skip qt_gui_cpp image_tools ' + data['test_args_default'],
         },
     }

commit 733e68f1319375b9069654cb5a0b804d51826a51

Fix CMake syntax for CentOS builds
Regression caused by fb403d185bab5188f8bb814db49ef76c2e3d88d8

Signed-off-by: Scott K Logan <logans@cottsay.net>

create_jenkins_job.py
@@ -121,7 +121,7 @@ def main(argv=None):
             'label_expression': 'linux',
             'shell_type': 'Shell',
             'build_args_default': '--packages-skip-by-dep qt_gui_cpp image_tools --packages-skip qt_gui_cpp image_tools ' + data['build_args_default'].replace(
-                '--cmake-args', '--cmake-args -DCMAKE_POLICY_DEFAULT_CMP0072=NEW -DPYTHON_VERSION=3.6 -DDISABLE_SANITIZERS'),
+                '--cmake-args', '--cmake-args -DCMAKE_POLICY_DEFAULT_CMP0072=NEW -DPYTHON_VERSION=3.6 -DDISABLE_SANITIZERS=ON'),
             'test_args_default': '--packages-skip-by-dep qt_gui_cpp image_tools --packages-skip qt_gui_cpp image_tools ' + data['test_args_default'],
         },
     }