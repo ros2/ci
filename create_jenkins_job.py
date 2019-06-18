5a2d8b4a012ed375c6d7950b3deffaace092d77a
Prajakta Gokhale <prajaktg@amazon.com>
2019-06-18 12:57:04 -0700

    Suppress gcc-7.x ABI incompatibility notes (#305)
    Signed-off-by: Prajakta Gokhale <prajaktg@amazon.com>
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -122,7 +122,7 @@ def main(argv=None):
             'shell_type': 'Shell',
             'ignore_rmw_default': data['ignore_rmw_default'] | {'rmw_connext_cpp', 'rmw_connext_dynamic_cpp'},
             'build_args_default': data['build_args_default'].replace(
-                '--cmake-args', '--cmake-args -DDISABLE_SANITIZERS=ON'),
+                '--cmake-args', '--cmake-args -DCMAKE_CXX_FLAGS=-Wno-psabi -DCMAKE_C_FLAGS=-Wno-psabi -DDISABLE_SANITIZERS=ON'),
         },
         'linux-centos': {
             'label_expression': 'linux',
