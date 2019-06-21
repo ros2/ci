ros2/ci

commit 9d78c3c88f46fc54b7f9ad564b1965cf6ad9f5af

Fix armhf packaging support (#307)
* Use OSRF image for armhf in packaging jobs

Follow-up to 6d7c6d41db00a56545907dc0004c56d2881c55c2

* Fix archive name when building 32-bit on 64-bit

Signed-off-by: Scott K Logan <logans@cottsay.net>

ros2_batch_job/packaging.py
@@ -159,7 +159,10 @@ def build_and_test_and_package(args, job):
     # create an archive
     folder_name = 'ros2-' + args.os
     if args.os == 'linux' or args.os == 'osx':
-        archive_path = 'ros2-package-%s-%s.tar.bz2' % (args.os, platform.machine())
+        machine = sys.implementation._multiarch.split('-', 1)[0]
+        if machine == 'arm':
+            machine = 'armhf'
+        archive_path = 'ros2-package-%s-%s.tar.bz2' % (args.os, machine)
 
         def exclude_filter(tarinfo):
             if tarinfo.isfile() and os.path.basename(tarinfo.name) == 'SOURCES.txt':

job_templates/packaging_job.xml.em
@@ -199,7 +199,11 @@ echo "# END SECTION"
 
 @[  if os_name in ['linux', 'linux-aarch64', 'linux-armhf', 'linux-centos']]@
 @[    if os_name in ['linux', 'linux-aarch64', 'linux-armhf']]@
+@[      if os_name == 'linux-armhf']@
+sed -i "s+^FROM.*$+FROM osrf/ubuntu_armhf:$CI_UBUNTU_DISTRO+" linux_docker_resources/Dockerfile
+@[      else]@
 sed -i "s+^FROM.*$+FROM ubuntu:$CI_UBUNTU_DISTRO+" linux_docker_resources/Dockerfile
+@[      end if]@
 export DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS} --build-arg UBUNTU_DISTRO=$CI_UBUNTU_DISTRO --build-arg ROS1_DISTRO=$CI_ROS1_DISTRO"
 @[    end if]@
 

commit d23c66167efc554d66c98014b7d4d650b7e18ed6

Fix OSX package names
Regression caused by 9d78c3c88f46fc54b7f9ad564b1965cf6ad9f5af

ros2_batch_job/packaging.py
@@ -159,9 +159,12 @@ def build_and_test_and_package(args, job):
     # create an archive
     folder_name = 'ros2-' + args.os
     if args.os == 'linux' or args.os == 'osx':
-        machine = sys.implementation._multiarch.split('-', 1)[0]
-        if machine == 'arm':
-            machine = 'armhf'
+        if args.os == 'osx':
+            machine = platform.machine()
+        else:
+            machine = sys.implementation._multiarch.split('-', 1)[0]
+            if machine == 'arm':
+                machine = 'armhf'
         archive_path = 'ros2-package-%s-%s.tar.bz2' % (args.os, machine)
 
         def exclude_filter(tarinfo):