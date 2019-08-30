b9aac044aacbd74eb83c93ae6ab532f87782ed82
Jacob Perron <jacob@openrobotics.org>
2019-08-29 17:45:23 -0700

    Add typed_ast Windows debug wheel
    Needed for running mypy linter in Windows debug builds.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -502,6 +502,7 @@ def run(args, build_function, blacklisted_package_names=None):
                     'https://github.com/ros2/ros2/releases/download/cryptography-archives/cryptography-2.7-cp37-cp37dm-win_amd64.whl',
                     'https://github.com/ros2/ros2/releases/download/lxml-archives/lxml-4.3.2-cp37-cp37dm-win_amd64.whl',
                     'https://github.com/ros2/ros2/releases/download/numpy-archives/numpy-1.16.2-cp37-cp37dm-win_amd64.whl',
+                    'https://github.com/ros2/ros2/releases/download/typed-ast-archives/typed_ast-1.4.0-cp37-cp37dm-win_amd64.whl',
                 ]
             else:
                 pip_packages += [
