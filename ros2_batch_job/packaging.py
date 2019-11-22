71c4722655c67ff3e285076f84d836a6b02124a0
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-11-22 11:18:02 -0800

    strip mention of (ROS 1) underlay workspace from setup.sh

--- ros2_batch_job/packaging.py
+++ ros2_batch_job/packaging.py
@@ -111,6 +111,21 @@ def build_and_test_and_package(args, job):
             print('no overlay packages specified')
         print('# END SUBSECTION')
 
+    # Only on Linux update the prefix level setup.sh to exclude the chained ROS 1 workspace used for the ros1_bridge
+    if args.os == 'linux':
+        print('# BEGIN SUBSECTION: remove ROS 1 underlay')
+        setup_file = os.path.join(args.installspace, 'setup.sh')
+        with open(setup_file, 'rb') as h:
+            content = h.read()
+        lines = content.splitlines()
+        start_index = lines.index(b'# source chained prefixes')
+        end_index = lines.index(b'# source this prefix')
+        # remove lines starting with start_index until before end_index
+        lines[start_index:end_index] = []
+        with open(setup_file, 'wb') as h:
+            for line in lines:
+                h.write(line + '\n')
+
     # Only on Linux and OSX Python scripts have a shebang line
     if args.os in ['linux', 'osx']:
         print('# BEGIN SUBSECTION: rewrite shebang lines')
