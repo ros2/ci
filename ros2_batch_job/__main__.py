867ac82191d4e7c15c0ac51d6d7adf7ba9e6a6dd
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-06-03 19:47:54 +0200

    Use --gcov options

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -309,8 +309,8 @@ def process_coverage(args, job):
             '-k',
             '-r', os.path.abspath('.'),
             '--xml', '--output=' + outfile,
-            '-e .*\.test\..*',
-            '-f .*\.' + coverage_package_name + '\..*',
+            '--gcov-exclude=.*#test#.*',
+            '--gcov-filter .*#' + coverage_package_name + '#.*',
             '-g']
         print(cmd)
         subprocess.run(cmd, check=True)
