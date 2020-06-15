e48fe5cc8755b3895025bc32140f71232bb2983a
Bo Sun <sunbo_1209@sina.com>
2020-06-16 01:08:32 +0800

    Update util.py

--- ros2_batch_job/util.py
+++ ros2_batch_job/util.py
@@ -199,15 +199,15 @@ class PipProtocol(AsyncSubprocessProtocol):
             self.progress_bar = True
         if b'[?25h' in data:
             for display_bar in self.progress_data:
-                sys.stdout.write(display_bar.decode('utf-8', 'replace').replace(os.linesep, '\n'))
+                sys.stdout.write(display_bar.decode('utf-8', 'replace').replace(os.linesep, '\r'))
             self.progress_bar = False
         if self.progress_bar:
             self.progress_data.append(data)
-            if len(self.progress_data) == 2:
-                sys.stdout.write(self.progress_data[-1].decode('utf-8', 'replace').replace(os.linesep, '\n'))
+            if len(self.progress_data) == 5:
+                sys.stdout.write(self.progress_data[-1].decode('utf-8', 'replace').replace(os.linesep, '\r'))
                 self.progress_data = []
         else:
-            sys.stdout.write(data.decode('utf-8', 'replace').replace(os.linesep, '\n'))
+            sys.stdout.write(data.decode('utf-8', 'replace').replace(os.linesep, '\r'))
 
 
     def on_stderr_received(self, data):
