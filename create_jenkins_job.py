473b055df97c8eb535ad80fad27a48d14d0cb78e
Scott K Logan <logans@cottsay.net>
2021-03-31 11:56:56 -0700

    Disable OpenSplice on RHEL
    I accidentally re-enabled this RMW while trying to drop what I thought
    was outdated cruft.

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -159,7 +159,7 @@ def main(argv=None):
     os_config_overrides = {
         'linux-rhel': {
             'mixed_overlay_pkgs': '',
-            'ignore_rmw_default': {'rmw_connext_cpp', 'rmw_connext_dynamic_cpp', 'rmw_connextdds'},
+            'ignore_rmw_default': data['ignore_rmw_default'] | {'rmw_connext_cpp', 'rmw_connext_dynamic_cpp', 'rmw_connextdds'},
             'use_connext_debs_default': 'false',
         },
     }
