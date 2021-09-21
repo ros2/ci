ros2/ci

commit 4f1b80b664cb7d9230ee562d06d0e9c73b7c46fd

Remove Dashing references from CI scripts. (#580)

ros2_batch_job/__main__.py
@@ -371,7 +371,7 @@ def build_and_test(args, job):
     # In Foxy and prior, xunit2 format is needed to make Jenkins xunit plugin 2.x happy
     # After Foxy, we introduced per-package changes to make local builds and CI
     # builds act the same.
-    if args.ros_distro in ('dashing', 'foxy'):
+    if args.ros_distro == 'foxy'
         pytest_args = ['-o', 'junit_family=xunit2']
         # We should only have one --pytest-args option, or some options might get ignored
         if '--pytest-args' in test_cmd:
@@ -520,24 +520,14 @@ def run(args, build_function, blacklisted_package_names=None):
             pip_packages += ['git+https://github.com/osrf/pyreadline']
 
             if args.cmake_build_type == 'Debug':
-                if args.ros_distro == 'dashing':
-                    pip_packages += [
-                        'https://github.com/ros2/ros2/releases/download/cryptography-archives/cffi-1.12.3-cp37-cp37dm-win_amd64.whl',  # required by cryptography
-                        'https://github.com/ros2/ros2/releases/download/cryptography-archives/cryptography-2.7-cp37-cp37dm-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/lxml-archives/lxml-4.3.2-cp37-cp37dm-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/netifaces-archives/netifaces-0.10.9-cp37-cp37dm-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/numpy-archives/numpy-1.16.2-cp37-cp37dm-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/typed-ast-archives/typed_ast-1.4.0-cp37-cp37dm-win_amd64.whl',  # required by mypy
-                    ]
-                else:
-                    pip_packages += [
-                        'https://github.com/ros2/ros2/releases/download/cryptography-archives/cffi-1.14.0-cp38-cp38d-win_amd64.whl',  # required by cryptography
-                        'https://github.com/ros2/ros2/releases/download/cryptography-archives/cryptography-2.9.2-cp38-cp38d-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/lxml-archives/lxml-4.5.1-cp38-cp38d-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/netifaces-archives/netifaces-0.10.9-cp38-cp38d-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/numpy-archives/numpy-1.18.4-cp38-cp38d-win_amd64.whl',
-                        'https://github.com/ros2/ros2/releases/download/typed-ast-archives/typed_ast-1.4.1-cp38-cp38d-win_amd64.whl',  # required by mypy
-                    ]
+                pip_packages += [
+                    'https://github.com/ros2/ros2/releases/download/cryptography-archives/cffi-1.14.0-cp38-cp38d-win_amd64.whl',  # required by cryptography
+                    'https://github.com/ros2/ros2/releases/download/cryptography-archives/cryptography-2.9.2-cp38-cp38d-win_amd64.whl',
+                    'https://github.com/ros2/ros2/releases/download/lxml-archives/lxml-4.5.1-cp38-cp38d-win_amd64.whl',
+                    'https://github.com/ros2/ros2/releases/download/netifaces-archives/netifaces-0.10.9-cp38-cp38d-win_amd64.whl',
+                    'https://github.com/ros2/ros2/releases/download/numpy-archives/numpy-1.18.4-cp38-cp38d-win_amd64.whl',
+                    'https://github.com/ros2/ros2/releases/download/typed-ast-archives/typed_ast-1.4.1-cp38-cp38d-win_amd64.whl',  # required by mypy
+                ]
             else:
                 pip_packages += [
                     f'cryptography{pip_cryptography_version}',

job_templates/ci_job.xml.em
@@ -132,8 +132,8 @@ if [ "$CI_USE_WHITESPACE_IN_PATHS" = "true" ]; then
   export CI_ARGS="$CI_ARGS --white-space-in sourcespace buildspace installspace workspace"
 fi
 export CI_ARGS="$CI_ARGS --ignore-rmw rmw_connext_dynamic_cpp"
-# TODO(asorbini) `rmw_connext_cpp` is still the default for dashing and foxy.
-if [ -n "$CI_ROS_DISTRO" -a \( "$CI_ROS_DISTRO" = dashing -o "$CI_ROS_DISTRO" = foxy \) ]; then
+# TODO(asorbini) `rmw_connext_cpp` is still the default for foxy.
+if [ -n "$CI_ROS_DISTRO" -a \( "$CI_ROS_DISTRO" = foxy \) ]; then
   if [ "$CI_USE_CONNEXTDDS" = "false" ]; then
     export CI_ARGS="$CI_ARGS rmw_connext_cpp"
   fi
@@ -277,15 +277,11 @@ if "!CI_USE_WHITESPACE_IN_PATHS!" == "true" (
   set "CI_ARGS=!CI_ARGS! --white-space-in sourcespace buildspace installspace workspace"
 )
 set "CI_ARGS=!CI_ARGS! --ignore-rmw rmw_connext_dynamic_cpp"
-:: TODO(asorbini) `rmw_connext_cpp` is still the default for dashing and foxy.
+:: TODO(asorbini) `rmw_connext_cpp` is still the default for foxy.
 set "CI_CONNEXTDDS_RMW="
 if "!CI_ROS_DISTRO!" NEQ "" (
-  if "!CI_ROS_DISTRO!" == "dashing" (
+  if "!CI_ROS_DISTRO!" == "foxy" (
     set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-  ) else (
-    if "!CI_ROS_DISTRO!" == "foxy" (
-      set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-    )
   )
 )
 if "!CI_CONNEXTDDS_RMW!" == "rmw_connext_cpp" (
@@ -390,15 +386,11 @@ if "!CI_USE_WHITESPACE_IN_PATHS!" == "true" (
   set "CI_ARGS=!CI_ARGS! --white-space-in sourcespace buildspace installspace workspace"
 )
 set "CI_ARGS=!CI_ARGS! --ignore-rmw rmw_connext_dynamic_cpp"
-:: TODO(asorbini) `rmw_connext_cpp` is still the default for dashing and foxy.
+:: TODO(asorbini) `rmw_connext_cpp` is still the default for foxy.
 set "CI_CONNEXTDDS_RMW="
 if "!CI_ROS_DISTRO!" NEQ "" (
-  if "!CI_ROS_DISTRO!" == "dashing" (
+  if "!CI_ROS_DISTRO!" == "foxy" (
     set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-  ) else (
-    if "!CI_ROS_DISTRO!" == "foxy" (
-      set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-    )
   )
 )
 if "!CI_CONNEXTDDS_RMW!" == "rmw_connext_cpp" (

job_templates/packaging_job.xml.em
@@ -146,8 +146,8 @@ if [ -n "${CI_COLCON_BRANCH+x}" ]; then
   export CI_ARGS="$CI_ARGS --colcon-branch $CI_COLCON_BRANCH"
 fi
 export CI_ARGS="$CI_ARGS --ignore-rmw rmw_connext_dynamic_cpp"
-# TODO(asorbini) `rmw_connext_cpp` is still the default for dashing and foxy.
-if [ -n "$CI_ROS_DISTRO" -a \( "$CI_ROS_DISTRO" = dashing -o "$CI_ROS_DISTRO" = foxy \) ]; then
+# TODO(asorbini) `rmw_connext_cpp` is still the default for foxy.
+if [ -n "$CI_ROS_DISTRO" -a \( "$CI_ROS_DISTRO" = foxy \) ]; then
   if [ "$CI_USE_CONNEXTDDS" = "false" ]; then
     export CI_ARGS="$CI_ARGS rmw_connext_cpp"
   fi
@@ -290,15 +290,11 @@ if "!CI_COLCON_BRANCH!" NEQ "" (
   set "CI_ARGS=!CI_ARGS! --colcon-branch !CI_COLCON_BRANCH!"
 )
 set "CI_ARGS=!CI_ARGS! --ignore-rmw rmw_connext_dynamic_cpp"
-:: TODO(asorbini) `rmw_connext_cpp` is still the default for dashing and foxy.
+:: TODO(asorbini) `rmw_connext_cpp` is still the default for foxy.
 set "CI_CONNEXTDDS_RMW="
 if "!CI_ROS_DISTRO!" NEQ "" (
-  if "!CI_ROS_DISTRO!" == "dashing" (
+  if "!CI_ROS_DISTRO!" == "foxy" (
     set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-  ) else (
-    if "!CI_ROS_DISTRO!" == "foxy" (
-      set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-    )
   )
 )
 if "!CI_CONNEXTDDS_RMW!" == "rmw_connext_cpp" (
@@ -393,15 +389,11 @@ if "!CI_ROS_DISTRO!" NEQ "" (
   set "CI_ARGS=!CI_ARGS! --ros-distro !CI_ROS_DISTRO!"
 )
 set "CI_ARGS=!CI_ARGS! --ignore-rmw rmw_connext_dynamic_cpp"
-:: TODO(asorbini) `rmw_connext_cpp` is still the default for dashing and foxy.
+:: TODO(asorbini) `rmw_connext_cpp` is still the default for foxy.
 set "CI_CONNEXTDDS_RMW="
 if "!CI_ROS_DISTRO!" NEQ "" (
-  if "!CI_ROS_DISTRO!" == "dashing" (
+  if "!CI_ROS_DISTRO!" == "foxy" (
     set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-  ) else (
-    if "!CI_ROS_DISTRO!" == "foxy" (
-      set CI_CONNEXTDDS_RMW=rmw_connext_cpp
-    )
   )
 )
 if "!CI_CONNEXTDDS_RMW!" == "rmw_connext_cpp" (

job_templates/snippet/property_parameter-definition_common.xml.em
@@ -60,7 +60,7 @@ choices.remove(ubuntu_distro)
             <a class="string-array">
               <string>@ros_distro</string>
 @{
-choices = ['dashing', 'foxy', 'galactic', 'rolling']
+choices = ['foxy', 'galactic', 'rolling']
 choices.remove(ros_distro)
 }@
 @[for choice in choices]@

commit 1b1720c343e31e239ea8b91cd4aab5d87fc6a071

Fix Python syntax error in batch job script
There was a missing colon introduced in #580.

Signed-off-by: Abrar Rahman Protyasha <aprotyas@u.rochester.edu>

ros2_batch_job/__main__.py
@@ -371,7 +371,7 @@ def build_and_test(args, job):
     # In Foxy and prior, xunit2 format is needed to make Jenkins xunit plugin 2.x happy
     # After Foxy, we introduced per-package changes to make local builds and CI
     # builds act the same.
-    if args.ros_distro == 'foxy'
+    if args.ros_distro == 'foxy':
         pytest_args = ['-o', 'junit_family=xunit2']
         # We should only have one --pytest-args option, or some options might get ignored
         if '--pytest-args' in test_cmd: