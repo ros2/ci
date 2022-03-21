424d57dd0bb168bf0ca470bfa66616332e5359a3
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-03-21 15:16:33 -0700

    Use venv from system instead of pinned virtualenv as venv provider.
    virtualenv 16 is broken with Python 3.10 and the pin has been around
    as long as it has due to virtualenv detection interfering with our
    install-scripts settings for libexec packages.
    
    As part of other patches to enable python3.10 support using a virtualenv
    has been moved completely off the table.
    
    If successful we'll have to compare this approach, which may _only_ work
    with virtualenv/python/setuptools above a certain threshold but it would
    then be on the table.

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -494,7 +494,7 @@ def run(args, build_function, blacklisted_package_names=None):
         venv_subfolder = 'venv'
         remove_folder(venv_subfolder)
         job.run([
-            sys.executable, '-m', 'virtualenv', '--system-site-packages',
+            sys.executable, '-m', 'venv', '--system-site-packages',
             '-p', sys.executable, venv_subfolder])
         venv_path = os.path.abspath(os.path.join(os.getcwd(), venv_subfolder))
         venv, venv_python = generated_venv_vars(venv_path)
