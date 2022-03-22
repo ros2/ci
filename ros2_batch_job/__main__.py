d0b3b3bb3d30faff112c02e3648d27f394a071fa
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-03-21 20:20:11 -0700

    Continue to use virtualenv with older python versions.
    Which also means continuing to install virtualenv (but let's try it
    systemways all around.)

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -493,8 +493,16 @@ def run(args, build_function, blacklisted_package_names=None):
 
         venv_subfolder = 'venv'
         remove_folder(venv_subfolder)
-        job.run([
-            sys.executable, '-m', 'venv', '--system-site-packages', venv_subfolder])
+        # With later versions of Python, setuptools, and venv, use the venv
+        # utility rather than virtualenv.
+        venv_cmd = [sys.executable, '-m']
+        if sys.version_info >= 10:
+            venv_cmd += ['venv']
+        else:
+            venv_cmd += ['virtualenv', '-p', sys.executable]
+        venv_cmd += ['--system-site-packages', venv_subfolder]
+
+        job.run(venv_cmd)
         venv_path = os.path.abspath(os.path.join(os.getcwd(), venv_subfolder))
         venv, venv_python = generated_venv_vars(venv_path)
         job.push_run(venv)  # job.run is now venv
