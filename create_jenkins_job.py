b034f48561067c426264ef3a4051f642b4fbeb36
Chris Lalancette <clalancette@openrobotics.org>
2023-01-26 17:29:44 -0500

    Remove all references to Galactic. (#687)
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -436,7 +436,6 @@ def main(argv=None):
         foxy_testing_pkgs_for_quality_level.remove('test_tracetools')
         foxy_testing_pkgs_for_quality_level.remove('rosidl_typesupport_introspection_tests')
         foxy_testing_pkgs_for_quality_level.append('tracetools_test')
-        galactic_testing_pkgs_for_quality_level = copy.copy(foxy_testing_pkgs_for_quality_level)
         humble_testing_pkgs_for_quality_level = copy.copy(testing_pkgs_for_quality_level)
 
         if os_name == 'linux':
@@ -482,20 +481,6 @@ def main(argv=None):
                                       ' --packages-up-to ' + ' '.join(quality_level_pkgs + foxy_testing_pkgs_for_quality_level),
                 'test_args_default': data['test_args_default'] +
                                      ' --packages-up-to ' + ' '.join(quality_level_pkgs + foxy_testing_pkgs_for_quality_level),
-            })
-            # Add a coverage job targeting Galactic.
-            create_job(os_name, 'nightly_' + os_name + '_galactic_coverage', 'ci_job.xml.em', {
-                'cmake_build_type': 'Debug',
-                'default_repos_url': 'https://raw.githubusercontent.com/ros2/ros2/galactic/ros2.repos',
-                'enable_coverage_default': 'true',
-                'time_trigger_spec': PERIODIC_JOB_SPEC,
-                'mailer_recipients': DEFAULT_MAIL_RECIPIENTS,
-                'ros_distro': 'galactic',
-                'ubuntu_distro': 'focal',
-                'build_args_default': data['build_args_default'] +
-                                      ' --packages-up-to ' + ' '.join(quality_level_pkgs + galactic_testing_pkgs_for_quality_level),
-                'test_args_default': data['test_args_default'] +
-                                     ' --packages-up-to ' + ' '.join(quality_level_pkgs + galactic_testing_pkgs_for_quality_level),
             })
             # Add a coverage job targeting Humble.
             create_job(os_name, 'nightly_' + os_name + '_humble_coverage', 'ci_job.xml.em', {
