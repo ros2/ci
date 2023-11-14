31b2b1ffc7882a9c4c0d57d1052ce0fd25487936
claraberendsen <claraberendsen@gmail.com>
2023-11-14 10:38:28 -0300

    ping to RHEL 9.1 waiting pyside2 rebuild on EPEL

--- create_jenkins_job.py
+++ create_jenkins_job.py
@@ -121,7 +121,7 @@ def main(argv=None):
         'dont_notify_every_unstable_build': 'false',
         'build_timeout_mins': 0,
         'ubuntu_distro': 'jammy',
-        'el_release': '9',
+        'el_release': '9.1',
         'ros_distro': 'rolling',
     }
 
