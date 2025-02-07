dd9ffb1419e1e83c9bcbb89588b96b929f9d1ad5
Chris Lalancette <clalancette@gmail.com>
2025-02-07 13:06:42 -0500

    Revert "Remove unnecessary shell=True."
    This reverts commit 381c5842a5a64f7fe731380e8bdf83464a3e5fc1.

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -461,7 +461,8 @@ def run(args, build_function, blacklisted_package_names=None):
 
         print('# BEGIN SUBSECTION: Print python versions')
         # Print setuptools version
-        job.run(['"%s"' % job.python, '-c', '"import setuptools; print(setuptools.__version__)"'])
+        job.run(['"%s"' % job.python, '-c', '"import setuptools; print(setuptools.__version__)"'],
+                shell=True)
 
         # Print the pip version
         job.run(['"%s"' % job.python, '-m', 'pip', '--version'], shell=True)
