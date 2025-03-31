a838748e19e75cfca12155c315baf6eac90c5f05
Juan Carlos Chaves Puertas <juanca@rti.com>
2025-04-01 00:26:47 +0200

    804: change to 7.3.0 packages

--- linux_docker_resources/entry_point.sh
+++ linux_docker_resources/entry_point.sh
@@ -42,26 +42,26 @@ if [ "${ARCH}" = "x86_64" -a "${ID}" = "ubuntu" ]; then
         case "${CI_ARGS}" in
           *--connext-debs*)
             echo "Using Debian package of Connext"
-            if test -r /opt/rti.com/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.sh; then
-                echo "Sourcing RTI setenv script /opt/rti.com/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.sh"
-                . /opt/rti.com/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.sh
+            if test -r /opt/rti.com/rti_connext_dds-7.3.0/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.sh; then
+                echo "Sourcing RTI setenv script /opt/rti.com/rti_connext_dds-7.3.0/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.sh"
+                . /opt/rti.com/rti_connext_dds-7.3.0/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.sh
             fi
             ;;
           *)
             echo "Installing Connext binaries off RTI website..."
-            if test -x /tmp/rticonnextdds-src/rti_connext_dds-6.0.1-pro-host-x64Linux.run; then
-                python3 -u /tmp/rti_web_binaries_install_script.py /tmp/rticonnextdds-src/rti_connext_dds-6.0.1-pro-host-x64Linux.run \
-                    /home/rosbuild/rti_connext_dds-6.0.1 --rtipkg_paths \
-                    /tmp/rticonnextdds-src/rti_connext_dds-6.0.1.25-pro-host-x64Linux.rtipkg \
-                    /tmp/rticonnextdds-src/rti_connext_dds-6.0.1.25-pro-target-x64Linux4gcc7.3.0.rtipkg \
-                    /tmp/rticonnextdds-src/openssl-1.1.1k-6.0.1.25-host-x64Linux.rtipkg \
-                    /tmp/rticonnextdds-src/rti_security_plugins-6.0.1.25-host-x64Linux.rtipkg \
-                    /tmp/rticonnextdds-src/rti_security_plugins-6.0.1.25-target-x64Linux4gcc7.3.0.rtipkg
+            if test -x /tmp/rticonnextdds-src/rti_connext_dds-7.3.0-pro-host-x64Linux.run; then
+                python3 -u /tmp/rti_web_binaries_install_script.py /tmp/rticonnextdds-src/rti_connext_dds-7.3.0-pro-host-x64Linux.run \
+                    /home/rosbuild/rti_connext_dds-7.3.0 --rtipkg_paths \
+                    /tmp/rticonnextdds-src/rti_connext_dds-7.3.0.7-pro-host-x64Linux.rtipkg \
+                    /tmp/rticonnextdds-src/rti_connext_dds-7.3.0.7-pro-target-x64Linux4gcc7.3.0.rtipkg \
+                    /tmp/rticonnextdds-src/openssl-3.0.12-7.3.0.7-host-x64Linux.rtipkg \
+                    /tmp/rticonnextdds-src/rti_security_plugins-7.3.0.7-host-x64Linux.rtipkg \
+                    /tmp/rticonnextdds-src/rti_security_plugins-7.3.0.7-target-x64Linux4gcc7.3.0.rtipkg
                 if [ $? -ne 0 ]; then
                     echo "Connext not installed correctly (maybe you're on an ARM machine?)." >&2
                     exit 1
                 fi
-                export CONNEXTDDS_DIR=/home/rosbuild/rti_connext_dds-6.0.1
+                export CONNEXTDDS_DIR=/home/rosbuild/rti_connext_dds-7.3.0
                 export RTI_OPENSSL_LIBS=$CONNEXTDDS_DIR/resource/app/lib/x64Linux2.6gcc4.4.5
             else
                 echo "No connext installation files found found." >&2
