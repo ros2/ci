db1d27c0ab813f04f7867a1c2ec6f2917744718a
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-04-10 16:17:53 -0700

    Typos.

--- windows_docker_resources/rti_web_binaries_install.ps1
+++ windows_docker_resources/rti_web_binaries_install.ps1
@@ -1,6 +1,6 @@
  if ( $Env:ROS_DISTRO -eq "jammy" -or $Env:ROS_DISTRO -eq "humble" ) {
 	pixi run 7z x -oC:\connext "C:\TEMP\rticonnextdds-src\openssl-1.1.1k-target-x64Win64VS2017.zip"
-	UUn& "C:\TEMP\rticonnextdds-src\rti_connext_dds-6.0.1-pro-host-x64Win64.exe" @(--mode unattended --unattendedmodeui minimalWithDialogs --prefix "C:\connext")
+	& "C:\TEMP\rticonnextdds-src\rti_connext_dds-6.0.1-pro-host-x64Win64.exe" @(--mode unattended --unattendedmodeui minimalWithDialogs --prefix "C:\connext")
 	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\openssl-1.1.1k-6.0.1.25-host-x64Win64.rtipkg"
 	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_connext_dds-6.0.1-pro-target-x64Win64VS2017.rtipkg"
 	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_security_plugins-6.0.1.25-host-x64Win64.rtipkg"
