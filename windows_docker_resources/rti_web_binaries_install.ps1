1fa02c019721d79c8b8242012a37cd40933ece9a
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-04-11 20:10:39 -0700

    Use cmd.exe to call batch scripts.

--- windows_docker_resources/rti_web_binaries_install.ps1
+++ windows_docker_resources/rti_web_binaries_install.ps1
@@ -3,10 +3,10 @@
  if ( $Env:ROS_DISTRO -eq "jammy" -or $Env:ROS_DISTRO -eq "humble" ) {
 	pixi run 7z x -oC:\connext "C:\TEMP\rticonnextdds-src\openssl-1.1.1k-target-x64Win64VS2017.zip"
 	& "C:\TEMP\rticonnextdds-src\rti_connext_dds-6.0.1-pro-host-x64Win64.exe" @("--mode", "unattended", "--unattendedmodeui", "minimalWithDialogs". "--prefix", "C:\connext")
-	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\openssl-1.1.1k-6.0.1.25-host-x64Win64.rtipkg"
-	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_connext_dds-6.0.1-pro-target-x64Win64VS2017.rtipkg"
-	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_security_plugins-6.0.1.25-host-x64Win64.rtipkg"
-	& $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_security_plugins-6.0.1.25-target-x64Win64VS2017.rtipkg"
+	cmd.exe /c $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\openssl-1.1.1k-6.0.1.25-host-x64Win64.rtipkg"
+	cmd.exe /c $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_connext_dds-6.0.1-pro-target-x64Win64VS2017.rtipkg"
+	cmd.exe /c $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_security_plugins-6.0.1.25-host-x64Win64.rtipkg"
+	cmd.exe /c $Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat -u "C:\TEMP\rticonnextdds-src\rti_security_plugins-6.0.1.25-target-x64Win64VS2017.rtipkg"
 } else {
 	$params = @("--mode", "unattended", "--unattendedmodeui", "minimalWithDialogs", "--prefix", "C:\connext")
 	& "C:\TEMP\rticonnextdds-src\rti_connext_dds-7.3.0-pro-host-x64Win64.exe" $params
@@ -16,9 +16,9 @@
 	$rti_security_host_params = @("-u",  "C:\TEMP\rticonnextdds-src\rti_security_plugins-7.3.0-host-x64Win64.rtipkg")
 	$rti_security_target_params = @("-u", "C:\TEMP\rticonnextdds-src\rti_security_plugins-7.3.0-target-x64Win64VS2017.rtipkg")
 
-	& "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $ssl_host_params
-	& "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $ssl_target_params
-	& "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $connext_pro_params
-	& "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $rti_security_host_params
-	& "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $rti_security_target_params
+	cmd.exe /c "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $ssl_host_params
+	cmd.exe /c "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $ssl_target_params
+	cmd.exe /c "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $connext_pro_params
+	cmd.exe /c "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $rti_security_host_params
+	cmd.exe /c "$Env:CONNEXTDDS_DIR\bin\rtipkginstall.bat" $rti_security_target_params
 }
