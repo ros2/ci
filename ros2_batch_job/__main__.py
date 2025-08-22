ros2/ci

commit ab3211670b8bfd3d62c3d3bacb008f822d35dd81

Add implicit dep from rviz_default_plugins as workaround (#822)
* Add implicit dep from rviz_default_plugins as workaround

This artificial dependency should prevent rviz_default_plugins from
being built at the same time as these other heavyweight packages.

This should eventually be reverted once we get the peak memory
utilization for these packages under control.

Signed-off-by: Scott K Logan <logans@cottsay.net>

* fixup! Add implicit dep from rviz_default_plugins as workaround

Signed-off-by: Scott K Logan <logans@cottsay.net>

---------

Signed-off-by: Scott K Logan <logans@cottsay.net>

ros2_batch_job/__main__.py
@@ -450,6 +450,15 @@ def run(args, build_function, blacklisted_package_names=None):
 
         colcon_script = which('colcon')
 
+        # Add workaround rviz_default_plugins resource utilization
+        with open('colcon.meta', 'w') as f:
+            f.writelines([
+                'names:',
+                '  rviz_default_plugins:',
+                '    build-dependencies:',
+                '      - rosbag2_performance_benchmarking',
+            ])
+
         # Fetch colcon mixins
         if args.colcon_mixin_url:
             print('# BEGIN SUBSECTION: Fetch colcon mixins')

commit dc5d0935226506e6da562ebfa9fa2837752fce68

Add missing newlines to colcon.meta file
Evidently writelines doesn't actually write line endings.

Fixes: ab3211670b8bfd3d62c3d3bacb008f822d35dd81
Signed-off-by: Scott K Logan <logans@cottsay.net>

ros2_batch_job/__main__.py
@@ -453,10 +453,10 @@ def run(args, build_function, blacklisted_package_names=None):
         # Add workaround rviz_default_plugins resource utilization
         with open('colcon.meta', 'w') as f:
             f.writelines([
-                'names:',
-                '  rviz_default_plugins:',
-                '    build-dependencies:',
-                '      - rosbag2_performance_benchmarking',
+                'names:\n',
+                '  rviz_default_plugins:\n',
+                '    build-dependencies:\n',
+                '      - rosbag2_performance_benchmarking\n',
             ])
 
         # Fetch colcon mixins